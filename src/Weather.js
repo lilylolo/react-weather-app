24bea6052f36f4ee7b634e4555c14eeb3700b367
lilylolo <78271372+lilylolo@users.noreply.github.com>
2021-04-02 20:33:39 +0100

    Update Icon URL to API

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       date: new Date (response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       humidity: response.data.main.humidity,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
