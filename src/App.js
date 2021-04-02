f72ee20412f507d90a1b962630a4a751eae171a5
lilylolo <78271372+lilylolo@users.noreply.github.com>
2021-04-02 17:23:53 +0100

    API intergration
    Installed axios and integrated API weather data for a default city

--- src/App.js
+++ src/App.js
@@ -1,93 +1,13 @@
 import './App.css';
+import Weather from './Weather'
 
 function App() {
   return (
     <div className="App">
     <div className="container">
-    <div className="App-wrapper">
 
-       <form>
-         <div className="row">
-           <div className="col-6">
-          <input type="text" placeholder="Enter a city.." autocomplete="off" />
-          </div>
-          <div className="col-3">
-          <input type="submit" value="Search"/>
-          </div>
-          <div className="col-3">
-          <input type="submit" value="Current"/>
-          </div>
-          </div>
-       </form>
-
-      <div className="row">
-      <div className="col-6">
-      <h1>London</h1>
-      <h2>16°C | F</h2>
-      </div>
-      <div className="col-6">
-        <p>Logo</p>
-      </div>
-      </div>
-
+      <Weather defaultCity="London" />
       
-      <p>
-        <ul>
-          <li>Description</li>
-          <li>Wind: 2km/s</li>
-          <li>Humidity: 44%</li>
-        </ul>
-      </p>
-
-    <div className="future-forecast">
-      <div className="row">
-      <div className="col-3">
-        Mon
-      </div>
-       <div className="col-3">
-        Tues
-      </div>
-       <div className="col-3">
-        Weds
-      </div>
-       <div className="col-3">
-        Thurs
-      </div>
-      </div>
-
-       <div className="row">
-      <div className="col-3">
-        Logo
-      </div>
-       <div className="col-3">
-        Logo
-      </div>
-       <div className="col-3">
-        Logo
-      </div>
-       <div className="col-3">
-        Logo
-      </div>
-      </div>
-
-      <div className="row">
-      <div className="col-3">
-        16°C
-      </div>
-       <div className="col-3">
-        16°C
-      </div>
-       <div className="col-3">
-        16°C
-      </div>
-       <div className="col-3">
-        16°C
-      </div>
-      </div>
-      </div>
-      
-
-    </div>
     <footer> <a href="https://github.com/lilylolo/react-weather-app" rel="noreferrer" target="_blank">Open-source</a> code by <a href="https://www.linkedin.com/in/lily-lo-9068aa12a" rel="noreferrer" target="_blank">Lily Lo</a></footer>
     </div>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,122 @@
+import React, { useState } from 'react';
+import './Weather.css';
+import axios from 'axios';
+
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({loaded: false});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      city: response.data.name,
+      date: "Saturday 1pm",
+      description: response.data.weather[0].description,
+      iconUrl: "",
+      humidity: response.data.main.humidity,
+      loaded: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      
+    })
+  }
+
+  if (weatherData.loaded) {
+     return  (
+    
+        <div className="App-wrapper">
+
+       <form>
+         <div className="row">
+           <div className="col-6">
+            <input type="text" placeholder="Enter a city.." autocomplete="off" />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search"/>
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Current"/>
+          </div>
+          </div>
+       </form>
+
+      <div className="row">
+      <div className="col-6">
+      <h1>{weatherData.city}</h1>
+      <p>{weatherData.date}</p>
+      <h2>{Math.round(weatherData.temperature)}°C | F</h2>
+      </div>
+      <div className="col-6">
+        <p><img src={weatherData.iconUrl}
+                alt={weatherData.description} /> </p>
+      </div>
+      </div>
+
+      
+      <p>
+        <ul>
+          <li className="text-capitalize">{weatherData.description}</li>
+          <li>Wind: {Math.round(weatherData.wind)}km/s</li>
+          <li>Humidity: {weatherData.humidity}%</li>
+        </ul>
+      </p>
+
+    <div className="future-forecast">
+      <div className="row">
+      <div className="col-3">
+        Mon
+      </div>
+       <div className="col-3">
+        Tues
+      </div>
+       <div className="col-3">
+        Weds
+      </div>
+       <div className="col-3">
+        Thurs
+      </div>
+      </div>
+
+       <div className="row">
+      <div className="col-3">
+        Logo
+      </div>
+       <div className="col-3">
+        Logo
+      </div>
+       <div className="col-3">
+        Logo
+      </div>
+       <div className="col-3">
+        Logo
+      </div>
+      </div>
+
+      <div className="row">
+      <div className="col-3">
+        16°C
+      </div>
+       <div className="col-3">
+        16°C
+      </div>
+       <div className="col-3">
+        16°C
+      </div>
+       <div className="col-3">
+        16°C
+      </div>
+      </div>
+
+      </div>
+      
+
+    </div>
+    );
+
+  } else {
+    const apiKey = "0d8a4461749ad27853c25b2011d4ab40";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "loading...";
+  }
+};
