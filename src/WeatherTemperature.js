7aec6b4b00bdc9eccc100c385b38c8da6622dfa3
lilylolo <78271372+lilylolo@users.noreply.github.com>
2021-04-26 17:06:39 +0100

    Fahrenheit conversion correct

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -3,9 +3,11 @@ import './WeatherTemperature.css'
 
 export default function WeatherTemperature(props) {
     const [unit, setUnit] = useState("celsius");
-    function convertToFarenheit(event) {
+
+
+    function convertToFahrenheit(event) {
         event.preventDefault();
-        setUnit("farenheit")
+        setUnit("fahrenheit")
     }       
     function convertToCelsius(event) {
         event.preventDefault();
@@ -15,15 +17,15 @@ export default function WeatherTemperature(props) {
     if (unit === "celsius") {
     return (
         <div>
-        <span className="temperature">{Math.round(props.celsius)}</span><span className="units"> °C | <a  href="/" onClick={convertToFarenheit}>F</a></span>
+        <span className="temperature">{Math.round(props.celsius)}</span><span className="units"> °C | <a  href="/" onClick={convertToFahrenheit}>F</a></span>
         </div>
     )
     } else {
-        let farenheit = (props.celsius * 9) /  + 32;
+        let fahrenheit = (props.celsius * 9) / 5 + 32;
 
         return ( 
             <div>
-        <span className="temperature">{Math.round(farenheit)}</span><span className="units"><a  href="/" onClick={convertToCelsius}> °C</a> | F</span>
+        <span className="temperature">{Math.round(fahrenheit)}</span><span className="units"><a  href="/" onClick={convertToCelsius}> °C</a> | F</span>
         </div>
  
         )
